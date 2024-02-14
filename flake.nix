46711241e863cf83a7477b796954faeff8b0956c
BeLeap <beleap@beleap.dev>
2024-02-14 12:47:49 +0900

    feat: setup devshell

--- flake.nix
+++ flake.nix
@@ -4,5 +4,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {}: {};
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShellNoCC {
+        nativeBuildInputs = with pkgs; [
+          clang
+        ];
+      };
+    }
+  );
 }
