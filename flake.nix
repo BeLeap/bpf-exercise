32f400a8a1d77995d1bbdf9ddb459a1faaa104f1
BeLeap <beleap@beleap.dev>
2024-02-14 12:33:42 +0900

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "eBPF Tutorial";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {}: {};
+}
